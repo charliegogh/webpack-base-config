3ac6f714300839b6caec7816f51f83b82ac9a6ab
charlie <e_rrol@163.com>
2019-12-09 18:07:24 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/**
+ * @author charlie
+ * @Description:
+ * webpack入口
+*/
+import './views/styles/reset.less'
+import './views/styles/about.less'
+
+import './views/about/about'
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = () => {
+    return new HTMLWebpackPlugin({
+        template: "./src/views/home/index.html",// 配置文件模板
+        filename: './home.html', // 配置输出文件名和路径
+        inject: true,
+        minify: {
+            removeComments: true,
+            collapseWhitespace: true
+        }
+    }),
+        new HTMLWebpackPlugin({
+            template: "./src/views/about/index.html",// 配置文件模板
+            filename: './about.html', // 配置输出文件名和路径
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
+            }
+        })
+}
